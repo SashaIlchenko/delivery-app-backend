64efd3d7b6ab583e94ebf4646a42dc584b6c40c7
SashaIlchenko <sandra.ilchenko11@gmail.com>
2023-06-02 16:14:08 +0300

    edit validation

--- routes/api/products.js
+++ routes/api/products.js
@@ -2,10 +2,8 @@ const express = require('express');
 
 const productController = require('../../controllers/products-controllers');
 
-const { validateBody } = require("../../middlewares");
-const { schemas } = require('../../models/products');
 const router = express.Router();
 
-router.get('/', validateBody(schemas.addSchema), productController.getAllProducts);
+router.get('/', productController.getAllProducts);
 
 module.exports = router;
