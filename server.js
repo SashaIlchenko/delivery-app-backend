41b702218a1bde4791b3af3951d0e174c856e68a
SashaIlchenko <sandra.ilchenko11@gmail.com>
2023-06-02 15:56:51 +0300

    edit env

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const mongoose = require("mongoose");
 
 const app = require('./app');
 
-const { DB_HOST, PORT = 3001 } = process.env;
+// const { DB_HOST, PORT = 3001 } = process.env;
 
 mongoose.set('strictQuery', true);
 
-mongoose.connect(DB_HOST)
+mongoose.connect("mongodb+srv://sandrabrovska:QhcCxRVOMKX7NM0N@cluster0.ngut3ky.mongodb.net/db-products?retryWrites=true&w=majority")
   .then(() => {
-    app.listen(PORT);
+    // app.listen(PORT);
     console.log("Database connection successful");
   })
   .catch(error => {
